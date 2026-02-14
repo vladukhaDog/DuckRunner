d80c2ac99ab9b36a7dea3faa148a66036b1b3472
vladukha <vladic.p@gmail.com>
2026-02-15 00:36:03 +0300

    Initial Commit

---
+++ DuckRunner/DuckRunnerApp.swift
@@ -0,0 +1,32 @@
+//
+//  DuckRunnerApp.swift
+//  DuckRunner
+//
+//  Created by vladukha on 15.02.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DuckRunnerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ DuckRunnerTests/DuckRunnerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DuckRunnerTests.swift
+//  DuckRunnerTests
+//
+//  Created by vladukha on 15.02.2026.
+//
+
+import Testing
+@testable import DuckRunner
+
+struct DuckRunnerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
